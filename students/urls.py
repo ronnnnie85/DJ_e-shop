35e9b2c10b3fa5ad0e0086ec7667386e0c5a82ed
MainUser <user@example.com>
2025-08-07 11:37:09 +0700

    html

--- students/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from students import views
-
-
-urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-]
