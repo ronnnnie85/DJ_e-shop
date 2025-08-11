c790debd5d27b7f5cc2734863fa6d22a331117a3
MainUser <user@example.com>
2025-08-11 09:23:03 +0700

    добавляю поля модели

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    # наименование,
+    # описание,
+    # изображение,
+    # категория,
+    # цена
+    # за
+    # покупку,
+    # дата
+    # создания,
+    # дата
+    # последнего
+    # изменения.
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование товара')
+    description = models.TextField(verbose_name='Описание', help_text='Введите описание товара')
+    picture = models.ImageField(upload_to='catalog/photo', blank=True, null=True, verbose_name='Изображение', help_text='Загрузите изображение товара')
