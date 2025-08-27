8882bf4408d70801fcb6b0931abbe78af567bdfa
MainUser <user@example.com>
2025-08-27 23:49:46 +0700

    сделал шаблоны и подшаблоны

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from catalog.models import Product
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context)
 
 
 def contacts(request):
