93c8edd933fa30abd416b10edf4374c59f54d95e
MainUser <user@example.com>
2025-08-31 20:00:46 +0700

    создал модель поста

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 
 class Post(models.Model):
-    header = models.CharField(max_length=200,
+    header = models.CharField(
+        max_length=200,
         verbose_name="Заголовок",
-        help_text="Введите заголовок",)
-    content = models.TextField(verbose_name="Содержимое", help_text="Введите содержимое")
+        help_text="Введите заголовок",
+    )
+    content = models.TextField(
+        verbose_name="Содержимое", help_text="Введите содержимое"
+    )
     preview = models.ImageField(
         upload_to="blog/images/",
         blank=True,
@@ -18,5 +22,19 @@ class Post(models.Model):
         verbose_name="Дата создания",
         help_text="Введите дату создания",
     )
-    publicated = models.BooleanField(verbose_name="Опубликовано", default=False, help_text="Укажите признак публикации")
-    number_of_views = models.IntegerField(verbose_name="Количество просмотров", help_text="Укажите количество просмотров")
+    publicated = models.BooleanField(
+        verbose_name="Опубликовано",
+        default=False,
+        help_text="Укажите признак публикации",
+    )
+    number_of_views = models.IntegerField(
+        verbose_name="Количество просмотров", help_text="Укажите количество просмотров"
+    )
+
+    class Meta:
+        verbose_name = "запись"
+        verbose_name_plural = "записи"
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"{self.header}"
