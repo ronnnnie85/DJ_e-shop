2cd3fd3cf687ddfd1c2dd93df44cb8a4e466ce90
MainUser <user@example.com>
2025-09-15 02:12:44 +0700

    CRUD для продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import ContactsView, ProductsListView, ProductsDetailView
+from catalog.views import ContactsView, ProductsListView, ProductsDetailView, ProductsCreateView, ProductsUpdateView, \
+    ProductsDeleteView
 
 app_name = CatalogConfig.name
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path('', ProductsListView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('products/<int:pk>/', ProductsDetailView.as_view(), name='product_details'),
+    path('products/new/', ProductsCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/edit/', ProductsUpdateView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', ProductsDeleteView.as_view(), name='product_delete'),
 ]
