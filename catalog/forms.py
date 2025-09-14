9449937d4ab28cde7b2c29cf9b4edcd5107208b2
MainUser <user@example.com>
2025-09-15 03:02:56 +0700

    сделал валидацию

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,19 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm, BooleanField
 
 from catalog.models import Product
+
+FORBIDDEN_WORDS = [
+    'казино',
+    'криптовалюта',
+    'крипта',
+    'биржа',
+    'дешево',
+    'бесплатно',
+    'обман',
+    'полиция',
+    'радар'
+]
 
 
 class StyleFormMixin:
@@ -17,3 +30,24 @@ class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
+
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price < 0:
+            raise ValidationError('Цена не может быть отрицательной')
+        return price
+
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if any(word in name.lower() for word in FORBIDDEN_WORDS) :
+            raise ValidationError('В имени запретное слово')
+        return name
+
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if any(word in description.lower() for word in FORBIDDEN_WORDS) :
+            raise ValidationError('В описании запретное слово')
+        return description
