e01059c29ce5dbf1c6aeadbe075bead3a348ea4d
MainUser <user@example.com>
2025-09-15 02:23:57 +0700

    сделал стилизацию

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,19 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 
 from catalog.models import Product
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs.update({'class': 'form-check-input'})
+            else:
+                field.widget.attrs.update({'class': 'form-control'})
+
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
